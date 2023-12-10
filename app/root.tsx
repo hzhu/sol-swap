699531a78009bdcabf30d5b2bcee468c563def04
hzhu <hz@henryzhu.me>
2023-12-09 19:46:28 -0800

    deps: add tailwind / react aria

--- app/root.tsx
+++ app/root.tsx
@@ -22,11 +22,18 @@ import {
 } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 
+import styles from "./tailwind.css";
+
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref
+    ? [
+        { rel: "stylesheet", href: cssBundleHref },
+        { rel: "stylesheet", href: styles },
+      ]
+    : []),
 ];
 
 export default function App() {
