075de7bddec3311e81697249fffeb7e54cfad3aa
hzhu <hz@henryzhu.me>
2023-12-10 14:17:19 -0800

    fix: refactor state to reduce double state when selecting token

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 [x] show users balance under sell input
-[] refactor state into useReducer
-[] bug when a user selects the opposite token, flip the two.
-[] max button populates the sell input
-[] successful swap... opens a modal dialog with details? and confetti.
-[] API calls are from our own backend
-[] show token image when token is selected
-[] combobox dropdown should have a single section saying "POPULAR" / "TRENDING" which will be hard-coded
-[] show token name as item text
-[] show token balance as item text
+[ ] async defer the token list
+[x] refactor state to reduce double state when selecting token
+[ ] refactor state into useReducer
+[ ] bug when a user selects the opposite token, flip the two.
+[ ] max button populates the sell input
+[ ] successful swap... opens a modal dialog with details? and confetti.
+[ ] API calls are from our own backend
+[ ] show token image when token is selected
+[ ] combobox dropdown should have a single section saying "POPULAR" / "TRENDING" which will be hard-coded
+[ ] show token name as item text
+[ ] show token balance as item text
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,256 +46,6 @@ const getProvider = () => {
   window.open("https://phantom.app/", "_blank");
 };
 
-const filteredList = [
-  {
-    address: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
-    chainId: 101,
-    decimals: 6,
-    name: "USD Coin",
-    symbol: "USDC",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "usd-coin" },
-  },
-  {
-    address: "So11111111111111111111111111111111111111112",
-    chainId: 101,
-    decimals: 9,
-    name: "Wrapped SOL",
-    symbol: "SOL",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
-    tags: ["old-registry"],
-    extensions: { coingeckoId: "wrapped-solana" },
-  },
-  {
-    address: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
-    chainId: 101,
-    decimals: 6,
-    name: "USDT",
-    symbol: "USDT",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB/logo.svg",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "tether" },
-  },
-  {
-    address: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
-    chainId: 101,
-    decimals: 5,
-    name: "BonkCoin",
-    symbol: "Bonk",
-    logoURI:
-      "https://arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I?ext=png",
-    tags: ["community"],
-    extensions: { coingeckoId: "bonk" },
-  },
-  {
-    address: "jtojtomepa8beP8AuQc6eXt5FriJwfFMwQx2v2f9mCL",
-    chainId: 101,
-    decimals: 9,
-    name: "JITO",
-    symbol: "JTO",
-    logoURI: "https://metadata.jito.network/token/jto/image",
-    tags: ["community"],
-    extensions: { coingeckoId: "jito-governance-token" },
-  },
-  {
-    address: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
-    chainId: 101,
-    decimals: 9,
-    name: "Marinade staked SOL (mSOL)",
-    symbol: "mSOL",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So/logo.png",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "msol" },
-  },
-  {
-    address: "J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn",
-    chainId: 101,
-    decimals: 9,
-    name: "Jito Staked SOL",
-    symbol: "JitoSOL",
-    logoURI: "https://storage.googleapis.com/token-metadata/JitoSOL-256.png",
-    tags: ["community", "solana-fm"],
-    extensions: { coingeckoId: "jito-staked-sol" },
-  },
-  {
-    address: "bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1",
-    chainId: 101,
-    decimals: 9,
-    name: "BlazeStake Staked SOL (bSOL)",
-    symbol: "bSOL",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1/logo.png",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "blazestake-staked-sol" },
-  },
-  {
-    address: "HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3",
-    chainId: 101,
-    decimals: 6,
-    name: "Pyth Network",
-    symbol: "PYTH",
-    logoURI: "https://pyth.network/token.svg",
-    tags: ["community"],
-    extensions: { coingeckoId: "pyth-network" },
-  },
-  {
-    address: "7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs",
-    chainId: 101,
-    decimals: 8,
-    name: "Ether (Portal)",
-    symbol: "ETH",
-    logoURI:
-      "https://raw.githubusercontent.com/wormhole-foundation/wormhole-token-list/main/assets/ETH_wh.png",
-    tags: ["wormhole", "old-registry"],
-    extensions: { coingeckoId: "ethereum-wormhole" },
-  },
-  {
-    address: "A9mUU4qviSctJVPJdBJWkb28deg915LYJKrzQ19ji3FM",
-    chainId: 101,
-    decimals: 6,
-    name: "USD Coin (Portal from Ethereum)",
-    symbol: "USDCet",
-    logoURI:
-      "https://raw.githubusercontent.com/wormhole-foundation/wormhole-token-list/main/assets/USDCet_wh.png",
-    tags: ["wormhole", "old-registry"],
-    extensions: { coingeckoId: "usd-coin-wormhole-from-ethereum" },
-  },
-  {
-    address: "AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR",
-    chainId: 101,
-    decimals: 5,
-    name: "Guacamole",
-    symbol: "GUAC",
-    logoURI:
-      "https://shdw-drive.genesysgo.net/36JhGq9Aa1hBK6aDYM4NyFjR5Waiu9oHrb44j1j8edUt/image.png",
-    tags: ["community"],
-    extensions: { coingeckoId: "guacamole" },
-  },
-  {
-    address: "orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE",
-    chainId: 101,
-    decimals: 6,
-    name: "Orca",
-    symbol: "ORCA",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE/logo.png",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "orca" },
-  },
-  {
-    address: "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R",
-    chainId: 101,
-    decimals: 6,
-    name: "Raydium",
-    symbol: "RAY",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R/logo.png",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "raydium" },
-  },
-  {
-    address: "n54ZwXEcLnc3o7zK48nhrLV4KTU5wWD4iq7Gvdt5tik",
-    chainId: 101,
-    decimals: 6,
-    name: "Peepo",
-    symbol: "PEEP",
-    logoURI:
-      "https://zk3y35n3ess4i2a4ya5a6hcllkelztul6a5vm2hk7wfjlq7fgypa.arweave.net/yreN9bskpcRoHMA6DxxLWoi8zovwO1Zo6v2KlcPlNh4",
-    tags: ["community"],
-    extensions: { coingeckoId: "peepo-sol" },
-  },
-  {
-    address: "27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4",
-    chainId: 101,
-    decimals: 6,
-    name: "Jupiter Perps",
-    symbol: "JLP",
-    logoURI: "https://static.jup.ag/jlp/icon.png",
-    tags: ["community"],
-    extensions: { coingeckoId: "jupiter-perpetuals-liquidity-provider-token" },
-  },
-  {
-    address: "MNDEFzGvMt87ueuHvVU9VcTqsAP5b3fTGPsHuuPA5ey",
-    chainId: 101,
-    decimals: 9,
-    name: "Marinade",
-    symbol: "MNDE",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/MNDEFzGvMt87ueuHvVU9VcTqsAP5b3fTGPsHuuPA5ey/logo.png",
-    tags: ["old-registry"],
-    extensions: { coingeckoId: "marinade" },
-  },
-  {
-    address: "HhJpBhRRn4g56VsyLuT8DL5Bv31HkXqsrahTTUCZeZg4",
-    chainId: 101,
-    decimals: 9,
-    name: "Myro",
-    symbol: "$MYRO",
-    logoURI: "https://imageupload.io/ib/JgSESETVx7v60lk_1698972645.png",
-    tags: ["community"],
-    extensions: { coingeckoId: "myro" },
-  },
-  {
-    address: "7kbnvuGBxxj8AG9qp8Scn56muWGaRaFqxg1FsRp3PaFT",
-    chainId: 101,
-    decimals: 6,
-    name: "UXD Stablecoin",
-    symbol: "UXD",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/7kbnvuGBxxj8AG9qp8Scn56muWGaRaFqxg1FsRp3PaFT/uxd-icon-black.png",
-    tags: ["old-registry"],
-    extensions: { coingeckoId: "uxd-stablecoin" },
-  },
-  {
-    address: "SHDWyBxihqiCj6YekG2GUr7wqKLeLAMK1gHZck9pL6y",
-    chainId: 101,
-    decimals: 9,
-    name: "Shadow Token",
-    symbol: "SHDW",
-    logoURI:
-      "https://shdw-drive.genesysgo.net/FDcC9gn12fFkSU2KuQYH4TUjihrZxiTodFRWNF4ns9Kt/250x250_with_padding.png",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "genesysgo-shadow" },
-  },
-  {
-    address: "LSTxxxnJzKDFSLr4dUkPcmCf5VyryEqzPLz5j4bpxFp",
-    chainId: 101,
-    decimals: 9,
-    name: "Liquid Staking Token",
-    symbol: "LST",
-    logoURI: "https://storage.googleapis.com/static-marginfi/lst.png",
-    tags: ["community"],
-    extensions: { coingeckoId: "liquid-staking-token" },
-  },
-  {
-    address: "hntyVP6YFm1Hg25TN9WGLqM12b8TQmcknKrdu1oxWux",
-    chainId: 101,
-    decimals: 8,
-    name: "Helium Network Token",
-    symbol: "HNT",
-    logoURI:
-      "https://shdw-drive.genesysgo.net/CsDkETHRRR1EcueeN346MJoqzymkkr7RFjMqGpZMzAib/hnt.png",
-    tags: ["community", "solana-fm"],
-    extensions: { coingeckoId: "helium" },
-  },
-  {
-    address: "7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU",
-    chainId: 101,
-    decimals: 9,
-    name: "Samoyed Coin",
-    symbol: "SAMO",
-    logoURI:
-      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU/logo.png",
-    tags: ["old-registry", "solana-fm"],
-    extensions: { coingeckoId: "samoyedcoin" },
-  },
-];
-
 function lamportsToTokenUnits(lamports: number, decimals: number) {
   return lamports / Math.pow(10, decimals);
 }
@@ -327,14 +77,7 @@ export default function Index() {
   const [quoteResponse, setQuoteResponse] = useState<any>(null);
   const [isSwapping, setIsSwapping] = useState<boolean>(false);
   const [transactionReceipt, setTransactionReceipt] = useState<string>("");
-  const [sellFieldState, setSellFieldState] = useState({
-    selectedKey: "So11111111111111111111111111111111111111112",
-    inputValue: "SOL",
-  });
-  const [buyFieldState, setBuyFieldState] = useState({
-    selectedKey: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
-    inputValue: "USDC",
-  });
+
   const [selectedSellToken, setSelectedSellToken] = useState({
     address: "So11111111111111111111111111111111111111112",
     chainId: 101,
@@ -367,7 +110,6 @@ export default function Index() {
       logoURI:
         "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
       tags: ["old-registry", "solana-fm"],
-      extensions: { coingeckoId: "usd-coin" },
     },
     {
       address: "So11111111111111111111111111111111111111112",
@@ -378,7 +120,6 @@ export default function Index() {
       logoURI:
         "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
       tags: ["old-registry"],
-      extensions: { coingeckoId: "wrapped-solana" },
     },
   ]);
 
@@ -413,8 +154,8 @@ export default function Index() {
       Number(debouncedSellAmount) * Math.pow(10, selectedSellToken.decimals);
 
     const params = {
-      inputMint: sellFieldState.selectedKey,
-      outputMint: buyFieldState.selectedKey,
+      inputMint: selectedSellToken.address,
+      outputMint: selectedBuyToken.address,
       amount: amountInSmallestUnit.toString(),
       slippageBps: "25",
       onlyDirectRoutes: "false",
@@ -439,13 +180,7 @@ export default function Index() {
     }
 
     fetchQuote();
-  }, [
-    debouncedSellAmount,
-    buyFieldState,
-    selectedBuyToken,
-    selectedSellToken,
-    sellFieldState,
-  ]);
+  }, [debouncedSellAmount, selectedBuyToken, selectedSellToken]);
 
   useEffect(() => {
     async function run() {
@@ -474,7 +209,7 @@ export default function Index() {
   const sellBalanceSPL = useMemo(() => {
     if (tokenAccounts) {
       const [balance] = tokenAccounts.value.filter((v) => {
-        return v.account.data.parsed.info.mint === sellFieldState.selectedKey;
+        return v.account.data.parsed.info.mint === selectedSellToken.address;
       });
 
       if (balance) {
@@ -483,12 +218,20 @@ export default function Index() {
     }
 
     return undefined;
-  }, [tokenAccounts, sellFieldState]);
+  }, [tokenAccounts, selectedSellToken]);
 
   const balanceUi =
     selectedSellToken.address === "So11111111111111111111111111111111111111112"
       ? nativeBalance
       : sellBalanceSPL;
+
+  const [sellInputValue, setSellInputValue] = useState<string>(
+    selectedSellToken.symbol
+  );
+
+  const [buyInputValue, setBuyInputValue] = useState<string>(
+    selectedBuyToken.symbol
+  );
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
@@ -497,31 +240,19 @@ export default function Index() {
         <ComboBox
           items={items}
           onInputChange={(value: string) => {
-            console.log(1, value);
-            setSellFieldState((prevState) => ({
-              inputValue: value,
-              selectedKey: value === "" ? "" : prevState.selectedKey,
-            }));
-
-            // filter the large list then set state for filtered list
-            const filteredList = tokenList
-              .filter((token) => {
-                return token.symbol.toLowerCase().includes(value.toLowerCase());
-              })
-              .slice(0, 7);
-            setItems(filteredList);
+            setSellInputValue(value);
+            const filteredList = tokenList.filter((token) => {
+              return token.symbol.toLowerCase().includes(value.toLowerCase());
+            });
+            setItems(filteredList.slice(0, 7));
           }}
-          inputValue={sellFieldState.inputValue}
-          selectedKey={sellFieldState.selectedKey}
+          inputValue={sellInputValue}
+          selectedKey={selectedSellToken.address}
           onSelectionChange={(id) => {
-            const selectedItem = filteredList.find((o) => o.address === id);
+            const selectedItem = items.find((o) => o.address === id);
             if (selectedItem) {
-              console.log(0, selectedItem);
               setSelectedSellToken(selectedItem);
-              setSellFieldState({
-                inputValue: selectedItem.symbol,
-                selectedKey: id as string,
-              });
+              setSellInputValue(selectedItem.symbol);
             }
           }}
         >
@@ -577,30 +308,18 @@ export default function Index() {
         <ComboBox
           items={items}
           onInputChange={(value: string) => {
-            setBuyFieldState((prevState) => ({
-              inputValue: value,
-              selectedKey: value === "" ? "" : prevState.selectedKey,
-            }));
-
-            // filter the large list then set state for filtered list
-            const filteredList = tokenList
-              .filter((token) => {
-                return token.symbol.toLowerCase().includes(value.toLowerCase());
-              })
-              .slice(0, 7);
-            setItems(filteredList);
-          }}
-          inputValue={buyFieldState.inputValue}
-          selectedKey={buyFieldState.selectedKey}
-          onSelectionChange={(id) => {
-            const selectedItem = filteredList.find((o) => o.address === id);
-            selectedItem && setSelectedBuyToken(selectedItem);
-
-            setBuyFieldState({
-              inputValue:
-                filteredList.find((o) => o.address === id)?.symbol ?? "",
-              selectedKey: id as string,
+            setBuyInputValue(value);
+            const newItems = tokenList.filter((token) => {
+              return token.symbol.toLowerCase().includes(value.toLowerCase());
             });
+            setItems(newItems.slice(0, 7));
+          }}
+          inputValue={buyInputValue}
+          selectedKey={selectedBuyToken.address}
+          onSelectionChange={(id) => {
+            const selectedItem = items.find((o) => o.address === id);
+            selectedItem && setSelectedBuyToken(selectedItem);
+            selectedItem && setBuyInputValue(selectedItem.symbol);
           }}
         >
           <Label>Chose a buy token</Label>
