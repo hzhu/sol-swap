50a4cd5995e714e1fae884fc865efd05c7daefbe
hzhu <hz@henryzhu.me>
2023-12-10 00:15:02 -0800

    feat: add a combobox

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,12 @@
+import {
+  Button,
+  ComboBox,
+  Input,
+  Label,
+  ListBox,
+  ListBoxItem,
+  Popover,
+} from "react-aria-components";
 import type { MetaFunction } from "@remix-run/node";
 import { useEffect, useRef, useState } from "react";
 import { Buffer } from "buffer";
@@ -7,6 +16,7 @@ import { Form } from "@remix-run/react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import type { PhantomWallet } from "~/types";
 import { set } from "@project-serum/anchor/dist/cjs/utils/features";
+import { tokenList } from "~/tokenList";
 
 // Extend the Window interface
 declare global {
@@ -38,6 +48,256 @@ const getProvider = () => {
 
   window.open("https://phantom.app/", "_blank");
 };
+
+const filteredList = [
+  {
+    address: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    chainId: 101,
+    decimals: 6,
+    name: "USD Coin",
+    symbol: "USDC",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "usd-coin" },
+  },
+  {
+    address: "So11111111111111111111111111111111111111112",
+    chainId: 101,
+    decimals: 9,
+    name: "Wrapped SOL",
+    symbol: "SOL",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+    tags: ["old-registry"],
+    extensions: { coingeckoId: "wrapped-solana" },
+  },
+  {
+    address: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
+    chainId: 101,
+    decimals: 6,
+    name: "USDT",
+    symbol: "USDT",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB/logo.svg",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "tether" },
+  },
+  {
+    address: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
+    chainId: 101,
+    decimals: 5,
+    name: "BonkCoin",
+    symbol: "Bonk",
+    logoURI:
+      "https://arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I?ext=png",
+    tags: ["community"],
+    extensions: { coingeckoId: "bonk" },
+  },
+  {
+    address: "jtojtomepa8beP8AuQc6eXt5FriJwfFMwQx2v2f9mCL",
+    chainId: 101,
+    decimals: 9,
+    name: "JITO",
+    symbol: "JTO",
+    logoURI: "https://metadata.jito.network/token/jto/image",
+    tags: ["community"],
+    extensions: { coingeckoId: "jito-governance-token" },
+  },
+  {
+    address: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+    chainId: 101,
+    decimals: 9,
+    name: "Marinade staked SOL (mSOL)",
+    symbol: "mSOL",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So/logo.png",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "msol" },
+  },
+  {
+    address: "J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn",
+    chainId: 101,
+    decimals: 9,
+    name: "Jito Staked SOL",
+    symbol: "JitoSOL",
+    logoURI: "https://storage.googleapis.com/token-metadata/JitoSOL-256.png",
+    tags: ["community", "solana-fm"],
+    extensions: { coingeckoId: "jito-staked-sol" },
+  },
+  {
+    address: "bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1",
+    chainId: 101,
+    decimals: 9,
+    name: "BlazeStake Staked SOL (bSOL)",
+    symbol: "bSOL",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1/logo.png",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "blazestake-staked-sol" },
+  },
+  {
+    address: "HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3",
+    chainId: 101,
+    decimals: 6,
+    name: "Pyth Network",
+    symbol: "PYTH",
+    logoURI: "https://pyth.network/token.svg",
+    tags: ["community"],
+    extensions: { coingeckoId: "pyth-network" },
+  },
+  {
+    address: "7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs",
+    chainId: 101,
+    decimals: 8,
+    name: "Ether (Portal)",
+    symbol: "ETH",
+    logoURI:
+      "https://raw.githubusercontent.com/wormhole-foundation/wormhole-token-list/main/assets/ETH_wh.png",
+    tags: ["wormhole", "old-registry"],
+    extensions: { coingeckoId: "ethereum-wormhole" },
+  },
+  {
+    address: "A9mUU4qviSctJVPJdBJWkb28deg915LYJKrzQ19ji3FM",
+    chainId: 101,
+    decimals: 6,
+    name: "USD Coin (Portal from Ethereum)",
+    symbol: "USDCet",
+    logoURI:
+      "https://raw.githubusercontent.com/wormhole-foundation/wormhole-token-list/main/assets/USDCet_wh.png",
+    tags: ["wormhole", "old-registry"],
+    extensions: { coingeckoId: "usd-coin-wormhole-from-ethereum" },
+  },
+  {
+    address: "AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR",
+    chainId: 101,
+    decimals: 5,
+    name: "Guacamole",
+    symbol: "GUAC",
+    logoURI:
+      "https://shdw-drive.genesysgo.net/36JhGq9Aa1hBK6aDYM4NyFjR5Waiu9oHrb44j1j8edUt/image.png",
+    tags: ["community"],
+    extensions: { coingeckoId: "guacamole" },
+  },
+  {
+    address: "orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE",
+    chainId: 101,
+    decimals: 6,
+    name: "Orca",
+    symbol: "ORCA",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE/logo.png",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "orca" },
+  },
+  {
+    address: "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R",
+    chainId: 101,
+    decimals: 6,
+    name: "Raydium",
+    symbol: "RAY",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R/logo.png",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "raydium" },
+  },
+  {
+    address: "n54ZwXEcLnc3o7zK48nhrLV4KTU5wWD4iq7Gvdt5tik",
+    chainId: 101,
+    decimals: 6,
+    name: "Peepo",
+    symbol: "PEEP",
+    logoURI:
+      "https://zk3y35n3ess4i2a4ya5a6hcllkelztul6a5vm2hk7wfjlq7fgypa.arweave.net/yreN9bskpcRoHMA6DxxLWoi8zovwO1Zo6v2KlcPlNh4",
+    tags: ["community"],
+    extensions: { coingeckoId: "peepo-sol" },
+  },
+  {
+    address: "27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4",
+    chainId: 101,
+    decimals: 6,
+    name: "Jupiter Perps",
+    symbol: "JLP",
+    logoURI: "https://static.jup.ag/jlp/icon.png",
+    tags: ["community"],
+    extensions: { coingeckoId: "jupiter-perpetuals-liquidity-provider-token" },
+  },
+  {
+    address: "MNDEFzGvMt87ueuHvVU9VcTqsAP5b3fTGPsHuuPA5ey",
+    chainId: 101,
+    decimals: 9,
+    name: "Marinade",
+    symbol: "MNDE",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/MNDEFzGvMt87ueuHvVU9VcTqsAP5b3fTGPsHuuPA5ey/logo.png",
+    tags: ["old-registry"],
+    extensions: { coingeckoId: "marinade" },
+  },
+  {
+    address: "HhJpBhRRn4g56VsyLuT8DL5Bv31HkXqsrahTTUCZeZg4",
+    chainId: 101,
+    decimals: 9,
+    name: "Myro",
+    symbol: "$MYRO",
+    logoURI: "https://imageupload.io/ib/JgSESETVx7v60lk_1698972645.png",
+    tags: ["community"],
+    extensions: { coingeckoId: "myro" },
+  },
+  {
+    address: "7kbnvuGBxxj8AG9qp8Scn56muWGaRaFqxg1FsRp3PaFT",
+    chainId: 101,
+    decimals: 6,
+    name: "UXD Stablecoin",
+    symbol: "UXD",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/7kbnvuGBxxj8AG9qp8Scn56muWGaRaFqxg1FsRp3PaFT/uxd-icon-black.png",
+    tags: ["old-registry"],
+    extensions: { coingeckoId: "uxd-stablecoin" },
+  },
+  {
+    address: "SHDWyBxihqiCj6YekG2GUr7wqKLeLAMK1gHZck9pL6y",
+    chainId: 101,
+    decimals: 9,
+    name: "Shadow Token",
+    symbol: "SHDW",
+    logoURI:
+      "https://shdw-drive.genesysgo.net/FDcC9gn12fFkSU2KuQYH4TUjihrZxiTodFRWNF4ns9Kt/250x250_with_padding.png",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "genesysgo-shadow" },
+  },
+  {
+    address: "LSTxxxnJzKDFSLr4dUkPcmCf5VyryEqzPLz5j4bpxFp",
+    chainId: 101,
+    decimals: 9,
+    name: "Liquid Staking Token",
+    symbol: "LST",
+    logoURI: "https://storage.googleapis.com/static-marginfi/lst.png",
+    tags: ["community"],
+    extensions: { coingeckoId: "liquid-staking-token" },
+  },
+  {
+    address: "hntyVP6YFm1Hg25TN9WGLqM12b8TQmcknKrdu1oxWux",
+    chainId: 101,
+    decimals: 8,
+    name: "Helium Network Token",
+    symbol: "HNT",
+    logoURI:
+      "https://shdw-drive.genesysgo.net/CsDkETHRRR1EcueeN346MJoqzymkkr7RFjMqGpZMzAib/hnt.png",
+    tags: ["community", "solana-fm"],
+    extensions: { coingeckoId: "helium" },
+  },
+  {
+    address: "7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU",
+    chainId: 101,
+    decimals: 9,
+    name: "Samoyed Coin",
+    symbol: "SAMO",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU/logo.png",
+    tags: ["old-registry", "solana-fm"],
+    extensions: { coingeckoId: "samoyedcoin" },
+  },
+];
 
 const url =
   "https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=1000000&slippageBps=30&onlyDirectRoutes=false&asLegacyTransaction=false&experimentalDexes=Jupiter%20LO";
@@ -116,8 +376,113 @@ export default function Index() {
     fetchQuote();
   }, [debouncedInputAmount]);
 
+  const [inputToken, setInputToken] = useState(
+    "So11111111111111111111111111111111111111112"
+  );
+
+  const [inputInputVal, setInputInputVal] = useState({
+    address: "So11111111111111111111111111111111111111112",
+    chainId: 101,
+    decimals: 9,
+    name: "Wrapped SOL",
+    symbol: "SOL",
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+    tags: ["old-registry"],
+    extensions: { coingeckoId: "wrapped-solana" },
+  });
+
+  let [fieldState, setFieldState] = useState({
+    selectedKey: "So11111111111111111111111111111111111111112",
+    inputValue: "SOL",
+  });
+
+  const [items, setItems] = useState([
+    {
+      address: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+      chainId: 101,
+      decimals: 6,
+      name: "USD Coin",
+      symbol: "USDC",
+      logoURI:
+        "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
+      tags: ["old-registry", "solana-fm"],
+      extensions: { coingeckoId: "usd-coin" },
+    },
+    {
+      address: "So11111111111111111111111111111111111111112",
+      chainId: 101,
+      decimals: 9,
+      name: "Wrapped SOL",
+      symbol: "SOL",
+      logoURI:
+        "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+      tags: ["old-registry"],
+      extensions: { coingeckoId: "wrapped-solana" },
+    },
+  ]);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <ComboBox
+        items={items}
+        onInputChange={(value: string) => {
+          setFieldState((prevState) => ({
+            inputValue: value,
+            selectedKey: value === "" ? "" : prevState.selectedKey,
+          }));
+
+          // filter the large list then set state for filtered list
+          const filteredList = tokenList
+            .filter((token) => {
+              return token.symbol.toLowerCase().includes(value.toLowerCase());
+            })
+            .slice(0, 7);
+          setItems(filteredList);
+        }}
+        inputValue={fieldState.inputValue}
+        selectedKey={fieldState.selectedKey}
+        onSelectionChange={(id) => {
+          setFieldState({
+            inputValue:
+              filteredList.find((o) => o.address === id)?.symbol ?? "",
+            selectedKey: id as string,
+          });
+        }}
+      >
+        <Label>Chose a sell token</Label>
+        <div>
+          <Input className="px-3 py-2" />
+          <Button>▼</Button>
+        </div>
+        <Popover>
+          <ListBox>
+            {(item: {
+              address: string;
+              chainId: number;
+              decimals: number;
+              name: string;
+              symbol: string;
+              logoURI: string;
+              tags: string[];
+              extensions: any;
+            }) => (
+              <ListBoxItem
+                key={item.address}
+                id={item.address}
+                className="px-1 py-1 cursor-pointer outline-none border-0 border-none rounded-md data-[hovered]:bg-blue-400 data-[hovered]:dark:bg-blue-marguerite-600 data-[hovered]:text-white data-[disabled]:bg-gray-100"
+              >
+                <img
+                  src={item.logoURI}
+                  alt={item.symbol}
+                  style={{ width: "1.5rem", height: "1.5rem" }}
+                />
+                <span>{item.symbol}</span>
+              </ListBoxItem>
+            )}
+          </ListBox>
+        </Popover>
+      </ComboBox>
       <h1>solswap</h1>
       <div>Pub key: {publicKey ? `✅ ${publicKey}` : "❌"}</div>
 
@@ -141,7 +506,10 @@ export default function Index() {
         />
         <button
           type="button"
+          disabled={!quoteResponse || isSwapping || !connected || !publicKey}
           onClick={async () => {
+            console.log(tokenList);
+            return;
             if (!quoteResponse) return;
 
             try {
